a92775bda8fe18925bfda075d2236cfe4087ab8e
FedorGorshkov <75787257+FedorGorshkov@users.noreply.github.com>
2021-01-20 22:02:18 +0500

    Разработка продолжается

--- main.py
+++ main.py
@@ -2,4 +2,53 @@
 
 
 import pygame as pg
-import objects
+from objects import *
+
+
+def read_map(file, N):
+    for i in range(N):
+        objects = list(file.readline().split())
+        for j in range(len(objects)):
+            # Обычные объекты на поле
+            if objects[j] == 'r':
+                pass
+            if objects[j] == 'b':
+                Tile((i, j), iron=False)
+            if objects[j] == 'i':
+                Tile((i, j), iron=True)
+            # Связанные объекты на поле, танки и базы
+            if objects[j] == 'C1':
+                Tower((i, j), 1)
+            if objects[j] == 'C2':
+                Tower((i, j), 2)
+            if objects[j] == 'T1':
+                Tank((i * 50, j * 50), 1, (0, 1))
+            if objects[j] == 'T2':
+                Tank((i * 50, j * 50), 2, (0, 1))
+    return
+
+
+selected_map, running, started = open('levels/level1.txt'), True, 0
+size = int(selected_map.readline())
+screen = pg.display.set_mode((850, 850))
+screen.fill((0, 255, 0))
+fon = load_image('Заставка.png')
+screen.blit(fon, (0, 0))
+pg.display.flip()
+read_map(selected_map, size)
+while running:
+    if started:
+        if started == 1:
+            started = 2
+            road = load_image('дорога.png')
+            for i in range(size):
+                for j in range(size):
+                    screen.blit(road, (i * 50, j * 50))
+        all_sprites.draw(screen)
+        pg.display.flip()
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        # Горячие клавиши
+        if event.type == pg.KEYUP and event.key == pg.K_SPACE:
+            started = 1
